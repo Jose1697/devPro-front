2f1c0fdb3bfcea05c040431ff4738cf22dd55ac8
unknown <bELMUT695>
2021-03-17 18:43:49 -0500

    Nabvar app.jsx v3

---
+++ src/components/ExpertoList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from '../components/Navbar'
+
+class ExpertoList extends React.Component{
+   render(){
+       return(
+     
+            
+        <div>
+            Lista de Mentores
+        </div>
+       
+       )
+   }
+}
+
+
+export default ExpertoList;
--- src/pages/Expertos.jsx
+++ src/pages/Expertos.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import Navbar from '../components/Navbar'
-
+import './styles/expert.css';
+import ExpertoList from '../components/ExpertoList'
 class Expertos extends React.Component{
    render(){
        return(
+        <div>   
+        <Navbar />      
+        <div >
+            <img    className="Expertos__container"    
+                src="https://www.unir.net/wp-content/uploads/2017/12/International_Mentoring_Program-1.jpg"             
+              />
+            </div>
+            <div class="texto-encima">
+                Red de Mentores</div>
+         
         <div>
-            
-        <Navbar />
-        <div>
-            gggggggggggggggsswwww
+        <ExpertoList/>
         </div>
          </div>
        )
