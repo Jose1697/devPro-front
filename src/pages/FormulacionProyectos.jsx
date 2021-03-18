76155c3e5fbdf37ca11d444219522b2441da4f8b
unknown <bELMUT695>
2021-03-18 17:23:40 -0500

    Nabvar MentoresLisT v3 Nosotros

---
+++ src/pages/FormulacionProyectos.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from '../components/Navbar'
+
+class FormulacionProyectos extends React.Component{
+   render(){
+       return(
+        <div>
+            
+        <Navbar />
+        <div>
+            prueba1
+        </div>
+         </div>
+       )
+   }
+}
+
+
+export default FormulacionProyectos;
--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -7,9 +7,37 @@ class Nosotros extends React.Component{
         <div>
             
         <Navbar />
-        <div>
-            prueba1
-        </div>
+        <div id="columnas" >
+            <li className="list-unstyled">
+                <div className="Centrado">
+                    <h1 >Integrantes</h1>
+                    
+                </div>
+            </li>
+            <li className="list-unstyled">
+              <img  className="Expertos__container"    
+                src="https://www.confiep.org.pe/wp-content/uploads/2019/06/team-3373638_1280-940x430.jpg"             
+              />
+            </li>
+           
+         </div>
+         <hr></hr>
+         <div id="columnas" >
+            <li className="list-unstyled">
+                <div className="Centrado">
+                    <h1 >Nuestra Visión</h1>
+                    
+                </div>
+            </li>
+           
+            <li className="list-unstyled">
+              <div className="Centrado">
+                    <h1 >Nuestra Misión</h1>
+                    
+                </div>
+            </li>
+           
+         </div>
          </div>
        )
    }
