0bc0020189f3e7ce792e52a6ab03c3409ed7ab66
unknown <bELMUT695>
2021-03-19 15:57:11 -0500

    Nabvar ProyectoLisT v3 Nosotros

--- src/components/ListProyectos.jsx
+++ src/components/ListProyectos.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
 import './styles/proyectList.css'
 
-
+class ListItaDocente extends React.Component {
+    render() {
+      return (
+        <div className="BadgesListItem">
+          <img
+            className="BadgesListItem__avatar"
+            src={this.props.badge.avatarUrl}
+            alt={`${this.props.badge.firstName} ${this.props.badge.lastName}`}
+          />
+  
+          <div>
+            <strong>
+              {this.props.badge.firstName} {this.props.badge.lastName}
+            </strong>
+            <br />@{this.props.badge.twitter}
+            <br />
+            {this.props.badge.jobTitle}
+          </div>
+        </div>
+      );
+    }
+  }
 class ListProyectos extends React.Component{
+  
    render(){
        return(
-        <div  className="Fondo">
-            
+     
+        <div >
        
+          {this.props.lista.map(badge => {
+            return (
+              <div id="caja1" key={badge.id}>
+                <ListItaDocente badge={badge} />
+              </div>
+            );
+          })}
        
-            
-         </div>
+      </div>
+       
        )
    }
 }
---
+++ src/components/Proyecto.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+class Proyecto extends React.Component{
+   render(){
+       return(
+        <div>
+          
+      
+       
+        <div>
+        Hola
+        </div>
+         </div>
+       )
+   }
+}
+
+
+export default Proyecto;
