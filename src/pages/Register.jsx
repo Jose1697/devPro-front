a7bf8f9158848a192cfbca1f500f07487af08a49
Jose1697 <josenetboy@hotmail.com>
2021-03-30 21:01:41 -0500

    registro

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,73 +1,84 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
-import '../components/styles/Register.css'
+import '../components/styles/Register.css';
 
 
-const Register = (props) => {
+class Register extends React.Component{
 
-    const [form, setValues] = useState({
-        email:'',
-        name:'',
-        password:'',
-    })
-
-    const handleInput= event => {
-        setValues({
-            ...form,
-            [event.target.name]: event.target.value
-        })
+    state = {
+        form:{first_name:'', last_name:'cliente' ,email:'',password:''}
     }
 
-    const handleSubmit = event => {
+    
+
+    handleInput= event => {
+        const formulario = this.state.form;
+        formulario[event.target.name] = event.target.value;
+        this.setState({form: formulario});
+    }
+
+    handleSubmit = event => {
         event.preventDefault()
-        console.log(form);
-        props.history.push('/')
+        fetch('https://devpro-2021.herokuapp.com/usuario/usuario/register/', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(this.state.form)
+        })
+        console.log(this.state.form);
+        // props.history.push('/')
     }
 
-    return(
-        <section className="register">
-            <div className="register__container">
-                
-                <h2>Regístrate</h2>
-                
-                <form className="register__container--form" onSubmit={handleSubmit}>
-                    <input
-                        name="name"
-                        className="input"  
-                        type="text" 
-                        placeholder="Nombre"
-                        onChange={handleInput}
-                    />
-                    <input
-                        name="email" 
-                        className="input"  
-                        type="text" 
-                        placeholder="Correo"
-                        onChange={handleInput}
-                    />
-                    <input
-                        name="password" 
-                        className="input"  
-                        type="password" 
-                        placeholder="Contraseña"
-                        onChange={handleInput}
-                    />
+    handleClick = (e) =>{
+        console.log("Button was clicked")
+    }
 
-                    <button className="button">Registrarme</button>
+    render() {
+        return(
+            <section className="register">
+                <div className="register__container">
                     
-                </form>
-                
+                    <h2>Regístrate</h2>
+                    
+                    <form className="register__container--form" onSubmit={this.handleSubmit}>
+                        <input
+                            name="first_name"
+                            className="input"  
+                            type="text" 
+                            placeholder="Nombre"
+                            onChange={this.handleInput}
+                        />
+                        <input
+                            name="email" 
+                            className="input"  
+                            type="text" 
+                            placeholder="Correo"
+                            onChange={this.handleInput}
+                        />
+                        <input
+                            name="password" 
+                            className="input"  
+                            type="password" 
+                            placeholder="Contraseña"
+                            onChange={this.handleInput}
+                        />
 
-                <p className="register__container--sesion">
-                    <Link to="/login">
-                        Iniciar Sesión
-                    </Link>
-                </p>
-                
-            </div>
+                        <button onClick={this.handleClick} className="button">Registrarme</button>
+                        
+                    </form>
+                    
 
-        </section>
-    )
-}
+                    <p className="register__container--sesion">
+                        <Link to="/login">
+                            Iniciar Sesión
+                        </Link>
+                    </p>
+                    
+                </div>
+
+            </section>
+        )
+
+    }
+};
 
 export default Register;
