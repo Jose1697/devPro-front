c2e71af40967df789c87014e99b170bd64438cc8
Jose1697 <josenetboy@hotmail.com>
2021-03-31 01:00:40 -0500

    modificacion navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,99 +1,134 @@
 import React  from 'react';
 import { Link } from 'react-router-dom';
-import './styles/Navbar.css';
+import './styles/Navbar.scss';
 import Logo from './images/logo-devpro.gif';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
+import logoUser  from './images/user-icon.png';
 
-function Navbar() {
+class Navbar extends React.Component {
 
-    
- 
-  return (
-    <>
-      <section className="header sticky-top">
-        <nav
-          id="header"
-          className="navbar navbar-expand-lg navbar-dark bg-black "
-        >
-          <div className="container">
-            <Link id="logo-header" className="navbar-brand" to="/">
-              <img className="logo"  src={Logo} alt="Logo" />
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
+  constructor(props){
+    super(props)
+    this.usuario = JSON.parse(localStorage.getItem('usuario'))
+  }
+
+  handleClick = event => {
+    localStorage.removeItem("usuario")
+  }
+  render(){
+      return (
+        <>
+          <section className="header sticky-top">
+            <nav
+              id="header"
+              className="navbar navbar-expand-lg navbar-dark bg-black "
             >
-              <span className="navbar-toggler-icon" />
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/">
-                  <FontAwesomeIcon icon={faHome} />
-                    Home<span className="sr-only">(current)</span>
-                  </Link>
-                </li>
-                <li className="nav-item">
+              <div className="container">
+                <Link id="logo-header" className="navbar-brand" to="/">
+                  <img className="logo"  src={Logo} alt="Logo" />
+                </Link>
+                <button
+                  className="navbar-toggler"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span className="navbar-toggler-icon" />
+                </button>
+                <div
+                  className="collapse navbar-collapse"
+                  id="navbarSupportedContent"
+                >
+                  <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">
+                      <FontAwesomeIcon icon={faHome} />
+                        Home<span className="sr-only">(current)</span>
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      
+                      <Link className="nav-link" to="/Nosotros">
+                        Nosotros
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/Emprendedor">
+                        Emprendedores
+                      </Link>
+                    </li>
+                    <li className="nav-item dropdown">
+                      <Link className="nav-link dropdown-toggle" to="/" id="navbardrop" data-toggle="dropdown">
+                        Empresas y Entidades
+                      </Link>
+                    
+                      <div className="dropdown-menu">
+                        <Link className="dropdown-item" to="#Formulacion">Formulacion de Proyectos</Link>
+                        <Link className="dropdown-item" to="#Talleres">Talleres y cursos de Innovacion</Link>
+                        <Link className="dropdown-item" to="#Talleres">Talleres y cursos de Innovacion</Link>
+                        <Link className="dropdown-item" to="#Talleres">Talleres y cursos de Innovacion</Link>
+                        <Link className="dropdown-item" to="#Talleres">Talleres y cursos de Innovacion</Link>
+                      
+                      </div>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/Proyectos">
+                        Proyectos
+                      </Link>
+                    </li>
                   
-                  <Link className="nav-link" to="/Nosotros">
-                    Nosotros
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/Emprendedor">
-                    Emprendedores
-                  </Link>
-                </li>
-                <li className="nav-item dropdown">
-                  <Link className="nav-link dropdown-toggle" to="/" id="navbardrop" data-toggle="dropdown">
-                    Empresas y Entidades
-                  </Link>
-                 
-                  <div className="dropdown-menu">
-                    <Link className="dropdown-item" to="#Formulacion">Formulacion de Proyectos</Link>
-                    <Link className="dropdown-item" to="#Talleres">Talleres y cursos de Innovacion</Link>
-                    <Link className="dropdown-item" to="#Talleres">Talleres y cursos de Innovacion</Link>
-                    <Link className="dropdown-item" to="#Talleres">Talleres y cursos de Innovacion</Link>
-                    <Link className="dropdown-item" to="#Talleres">Talleres y cursos de Innovacion</Link>
-                   
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/Proyectos">
-                    Proyectos
-                  </Link>
-                </li>
-               
-                <li className="nav-item">
-                  <Link className="nav-link" to="/Expertos">
-                    Expertos
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/Contacto">
-                    Contacto
-                  </Link>
-                </li>
-                <li className="sesion">
-                  <Link  className="btn btn-success" to="/login">Iniciar Sesion</Link>
-                </li>
-               
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </section>
-    </>
-  );
-}
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/Expertos">
+                        Expertos
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/Contacto">
+                        Contacto
+                      </Link>
+                    </li>
+                    {/*  */}
+                    {this.usuario != null ? 
+                      <div className="header__menu">
+                          <div className="header__menu--profile">
+                              
+                              <p>{this.usuario.first_name}</p>
+                              
+                              {this.usuario.last_name === '' ?
+                                <img src={logoUser} alt="User"/> :
+
+                                <img className="fotoperfil" src={logoUser} alt="User"/>
+
+                              }
+                    
+                          </div>
+                    
+                          <ul className="opciones">
+                              <li><Link  to="/" >Cuenta</Link></li>
+                              <li><Link onClick={this.handleClick}  to="/login">Cerrar Sesión</Link></li>
+                          </ul>
+                      </div> :
+
+                      
+                      <Link className="btn btn-success" to="login">Iniciar Sesion</Link>
+                      
+                  
+                    }
+                    
+
+                    
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </section>
+        </>
+      );
+  }
+};
 
 export default Navbar;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, {useState}from 'react';
+import React from 'react';
 import '../components/styles/Login.css'
 import {Link} from 'react-router-dom';
 
