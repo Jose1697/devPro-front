8ccca1b02274ca4cd447cc030b4d397711e0db2e
Jose1697 <josenetboy@hotmail.com>
2021-03-30 21:56:50 -0500

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,67 +3,83 @@ import '../components/styles/Login.css'
 import {Link} from 'react-router-dom';
 
 
-const Login = (props) => {
+class Login extends React.Component{
 
-    const [form, setValues] = useState({
-        email:'',
-    })
+    state = {
+        form:{email:'', password:''}
+    }
 
-    const handleInput = event => {
-        setValues({
-            ...form,
-            [event.target.name]: event.target.value
+    handleInput = event => {
+        const formulario = this.state.form;
+        formulario[event.target.name] = event.target.value;
+        this.setState({form: formulario});
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        fetch('https://devpro-2021.herokuapp.com/usuario/usuario/login/', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(this.state.form)
         })
+        .then( data => data.json())
+        .then( data => {
+            if(data.Usuario){
+                alert(`${data.Usuario.first_name} se ha logueado correctamente`)
+                this.props.history.push('/')
+            }else{
+                alert('El email o contraseña son incorrectos')
+            }
+        })
+        //console.log(this.state.form);
+        // this.props.history.push('/')
+
     }
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        console.log(form);
-        props.history.push('/')
+    render() {
 
-    }
-
-    return (
-        <section className="login">
-        <div className="login__container">
-            
-            <h2 className="title">Inicia Sesión</h2>
-            
-            <form className="login__container--form" onSubmit={handleSubmit}>
-                <input 
-                    name="email"
-                    className="input"  
-                    type="text" 
-                    placeholder="Correo"
-                    onChange={handleInput}
-                />
-
-                <input 
-                    email="password"
-                    className="input"  
-                    type="password" 
-                    placeholder="Contraseña"
-                    onChange={handleInput}
-                />
-                <button className="button">Iniciar sesión</button>
-                <div className="login__container--remember">
-                    <label>
-                        <input type="checkbox" name="" id="cbox1" value="checkbox"/>Recuérdame
-                    </label>
-                    <Link to="">Olvidé mi contraseña</Link>
-                </div>
-            </form>
-            
-            <p className="login__container--register">No tienes ninguna cuenta 
-                <Link to="/register">
-                    Registrate
-                </Link>
+        return (
+            <section className="login">
+            <div className="login__container">
                 
-            </p>
-        </div>
+                <h2 className="title">Inicia Sesión</h2>
+                
+                <form className="login__container--form" onSubmit={this.handleSubmit}>
+                    <input 
+                        name="email"
+                        className="input"  
+                        type="text" 
+                        placeholder="Correo"
+                        onChange={this.handleInput}
+                    />
 
-    </section>
-    );
-}
+                    <input 
+                        name="password"
+                        className="input"  
+                        type="password" 
+                        placeholder="Contraseña"
+                        onChange={this.handleInput}
+                    />
+                    <button className="button">Iniciar sesión</button>
+                    <div className="login__container--remember">
+                        <label>
+                            <input type="checkbox" name="" id="cbox1" value="checkbox"/>Recuérdame
+                        </label>
+                        <Link to="">Olvidé mi contraseña</Link>
+                    </div>
+                </form>
+                
+                <p className="login__container--register">No tienes ninguna cuenta 
+                    <Link to="/register">
+                        Registrate
+                    </Link>
+                    
+                </p>
+            </div>
+
+        </section>
+        );
+    }
+};
 
 export default Login;
