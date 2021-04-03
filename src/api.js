a00ca07d0c22120ffd18f8b35cb99597d4a24e62
unknown <bELMUT695>
2021-04-03 10:59:27 -0500

    Experto api.js

---
+++ src/api.js
@@ -0,0 +1,30 @@
+const BASE_URL = 'http://localhost:8000';
+
+async function callApi(endpoint, options = {}) {
+   
+  
+    options.headers = {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    };
+  
+    const url = BASE_URL + endpoint;
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data;
+    
+  }
+
+const api = {
+    badges: {
+      list() {
+        return callApi('/core/experto');
+      },
+    },
+};
+
+
+
+
+
+  export default api;
