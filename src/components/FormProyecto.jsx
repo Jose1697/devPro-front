18bee38792608458d3bb28aecd09a79b1ce41c30
unknown <bELMUT695>
2021-04-13 21:27:13 -0500

    FormProyecto

--- src/components/FormProyecto.jsx
+++ src/components/FormProyecto.jsx
@@ -6,7 +6,47 @@ class FormProyecto extends React.Component{
          <div>
              <Navbar /> 
               
-              Crear un proyecto
+             <h1 className="Titulo">Empieza un Proyecto</h1>
+             <div className="form_expert">
+        
+
+            <form className="form mt-10" style={{width: '95%'}}  >
+                <div className="card-body form_Ex">   
+                   
+
+
+                   
+                    <div className="form-group">
+                        <label className="dni">Nombre Proyecto</label>
+                        <input
+                         className="form-control"
+                        />
+                    </div>
+                    
+                    <div className="form-group">
+                        <label className="Descripcion">Descripcion Proyecto</label>
+                        <input
+                         className="form-control"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="Descripcion">Financiamiento Proyecto</label>
+                        <input
+                         className="form-control"
+                        />
+                    </div>
+                 
+                    <div>
+                        
+                        <button  className="btn btn-primary btn-xs">
+                             <div className="botonR">Aun Falta
+                               </div>  
+                        </button>
+                    </div>
+                    
+                </div>   
+            </form>
+      </div>
 						
          </div>
        )
