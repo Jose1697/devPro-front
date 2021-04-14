a6d783d189d4b08a68c68318212f5fbc212b8486
unknown <bELMUT695>
2021-04-14 15:28:17 -0500

    FormProyecto

--- src/pages/NewProyecto.jsx
+++ src/pages/NewProyecto.jsx
@@ -2,18 +2,30 @@ import React from 'react';
 import Navbar from '../components/Navbar'
 import FormProyecto from '../components/FormProyecto'
 import './styles/expert.css';
+import Glyphicon from '@strongdm/glyphicon'
 class NewProyecto extends React.Component{
    render(){
        return(
          <div>
             
             <Navbar />
-                <div >
-                    Convierte ....en realidad
-                     <br/>
-                     <br/>
-                    <a class="btn btn-primary" href="/newPro" role="button">Empieza Un Proyecto</a>
-                </div>
+            <div className="wrapper bgded overlay" >
+                <div id="pageintro" className="hoc"> 
+                
+                    <article>
+                        <p>Mauris placerat sem hendrerit</p>
+                        <h3 className="heading">Convierte tu proyecto creativo en realidad.</h3>
+                        <p>Eu elit lacinia porta in eget risus duis pretium tellus ac odio</p>
+                   
+                        <a class="btn btn-primary" href="/newPro" role="button" >
+                            Empieza un 
+                               Proyecto
+                                <span className="glyphicon glyphicon-chevron-right"></span>  
+                        </a>
+                    </article>
+   
+  </div>
+</div>
              
 						
          </div>
