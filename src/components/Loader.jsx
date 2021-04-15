20d18ba41ca03176390feb3ac1c43bb3de4250dd
unknown <bELMUT695>
2021-04-15 14:38:53 -0500

    Buscador Terminado

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
 import './styles/Loader.css';
-
+import Navbar from './Navbar'
 export default class Loader extends Component {
   render() {
     return (
+      <>
+      
       <div className="lds-grid">
+         
         <div />
         <div />
         <div />
@@ -16,6 +19,7 @@ export default class Loader extends Component {
         <div />
         <div />
       </div>
+      </>
     );
   }
 }
--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './styles/PageLoading.css';
 import Loader from './Loader';
-
+import Navbar from './Navbar'
 function loading() {
    return (
-      <div className="PageLoading">
+      <>
+      <Navbar />
+      <div className="PageLoading">       
          <Loader/>
       </div>
-     
+     </>
     
    );
 }
