b0514e69335b1e037d4c790ed203a6478540452a
Jose1697 <josenetboy@hotmail.com>
2021-04-20 21:41:10 -0500

    emprendores

--- src/components/EmprendedorList.jsx
+++ src/components/EmprendedorList.jsx
@@ -1,16 +1,48 @@
 import React from 'react';
+import './styles/expertList.css';
 
-class EmprendedorList extends React.Component{
+class EmprendedorItemList extends React.Component{
    render(){
        return(
-         <div>
-            
-              
-                prueba10
-						
-         </div>
+          <div className="BadgesListItem">
+
+            <img
+              className="BadgesListItem__avatar"
+              src={this.props.expert.usuario.photo}
+              alt="experto"
+            />
+    
+            <div className="datos">
+              <strong>
+                {this.props.expert.usuario.first_name} {this.props.expert.usuario.last_name}
+              </strong>
+              <br />{this.props.expert.usuario.email}
+              <br />{this.props.expert.descripcion_experto}
+            </div>
+          </div>
        )
    }
+}
+
+class EmprendedorList extends React.Component{
+  
+  render(){
+      return(
+    
+       <div >
+         <br/>
+         {this.props.lista.map(experto => {
+           return (
+             <div id="caja1" key={experto.usuario}>
+               {<EmprendedorItemList expert={experto} />}
+             </div>
+           );
+         })}
+      
+     </div>
+      
+      )
+  }
 }
 
 
--- src/pages/Emprendedor.jsx
+++ src/pages/Emprendedor.jsx
@@ -1,20 +1,80 @@
 import React from 'react';
 import Navbar from '../components/Navbar'
-import EmprendedorList from '../components/EmprendedorList'
 import './styles/expert.css';
+import Footer from '../components/Footer';
+import EmprendedorList from '../components/EmprendedorList'
+import PageLoader from '../components/loading';
+
 class Emprendedor extends React.Component{
-   render(){
-       return(
-         <div>
+  state = {
+     
+    loading: true,
+    error: null,
+    data1:undefined,
+  };
+
+  componentDidMount(){
+    this.fetchData()
+  }
+
+  fetchData=async () => {
+    this.setState({loading:true,error:null})
+
+    try{
+     const data1=await fetch('https://devpro-2021.herokuapp.com/core/cliente/')
+     const clientes = await data1.json()
+    
+     
+     this.setState({loading:false,data1:clientes})
+    }catch(error){
+     this.setState({loading:false,error:error})
+    }
+  }
+
+  
+  
+  render(){
+    if (this.state.loading === true) {
+      return <PageLoader  />;
+    }
+
+    return(
+      <>
+        <div className="container__expert">   
+        <Navbar />      
+        <div className="container__expert1">
+            <img className="Expertos__container"    
+                src="https://www.unir.net/wp-content/uploads/2017/12/International_Mentoring_Program-1.jpg"
+                alt=""             
+              />
+              <div class="texto-encima">
+                Red de Emprendedores
+            </div>
+        </div>
+        
             
-                <Navbar />
-               <div>
-               <EmprendedorList />
-               </div>
-						
-         </div>
-       )
-   }
+            <div >
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <h1 className="tituloPrin">Lista de Emprendedores</h1>
+                 {<h1 className="titulo2"></h1> }
+            </div>
+         
+        <div className="expert_container">
+           <div >
+           <EmprendedorList lista={this.state.data1}/>
+           </div>
+           <Footer/>
+        </div>
+       
+        </div>
+         
+        </>
+    )
+  }
 }
 
 
